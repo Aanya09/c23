d7cb201d262c881777a9e27a03cd5f9d914239cc
Aanya09 <71958511+Aanya09@users.noreply.github.com>
2020-10-18 11:36:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c23
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,29 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var engine,world;
+
+var box1;
+var ground;
+var box2;
+
+
+function setup() {
+  createCanvas(400,400);
+ engine=Engine.create();
+ world=engine.world;
+ box1=new Box(200,200,50,50);
+ ground=new Ground(200,380,400,20);
+ box2=new Box(240,100,50,100);
+
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine)
+ box1.display();
+ ground.display();
+ box2.display();
+ 
+}
